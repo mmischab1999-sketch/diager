66ed008a4e829c880e94f3859b045615227e102f
mike <mmischab1999@gmail.com>
2026-05-02 17:34:37 +0500

    Add files via upload

--- diager.psm1
+++ diager.psm1
@@ -1,4 +1,4 @@
-$script:diagerBannerShown = $false
+
 
 $prefix = "Diager >>> "
 
@@ -25,30 +25,30 @@ if (-not $health -and -not $defrag -and -not $free) {
 	
 if ($free) {
 	write-host "$prefix" -nonewline
-	write-host "[GETTING]:" -f cyan -nonewline
+	write-host " [GETTING]:" -f cyan -nonewline
 	write-host "Your disks free space..."
 start-sleep -seconds 1
 	#если инфа о дисках получена 
 	if ($drives -ne $null) {
 		write-host "$prefix" -nonewline
-		write-host "[GOT]:" -f green -nonewline
+		write-host " [GOT]:" -f green -nonewline
 		write-host "Your disks free space" 
 		$Drives | % {
         		if ($_.Free -gt 5GB) {
 				$currentGB = [math]::Round($_.free / 1GB, 2)
         			write-host "$prefix" -nonewline
-				write-host "[OK]:" -f green -nonewline
+				write-host " [OK]:" -f green -nonewline
 				write-host "$_ free space is normal. It`s $currentGB GB"
        			 } else {
 				write-host "$prefix" -nonewline
-				write-host "[WARNING]:" -f yellow -nonewline
+				write-host " [WARNING]:" -f yellow -nonewline
 				write-host "Less than 5GB free space on $_ disk! Use utilits to clean or delete files you don`t need"
 			}
 		}
 	#если инфа о дисках не получена
 	} else {
 		write-host "$prefix" -nonewline
-		write-host "[COULDN`T]:" -f red -nonewline
+		write-host " [COULDN`T]:" -f red -nonewline
 		write-host "Get your disks free space"
 	}
 	} #закрыл парам
@@ -57,61 +57,62 @@ start-sleep -seconds 1
 
 	if ($health) {
 	write-host "$prefix" -nonewline
-	write-host "[GETTING]:" -f cyan -nonewline 
+	write-host " [GETTING]:" -f cyan -nonewline 
 	write-host "Your disks health status..."
 start-sleep -seconds 1
 	if ($Volumes.DriveLetter -ne $null) {
 		write-host "$prefix" -nonewline
-		write-host "[GOT]:" -f green -nonewline
+		write-host " [GOT]:" -f green -nonewline
 		write-host "Your disks health status"
 		$Volumes | % {
 			if ($_.HealthStatus -eq "Healthy") {
 				write-host "$prefix" -nonewline
-				write-host "[OK]:" -f green -nonewline
+				write-host " [OK]:" -f green -nonewline
 				write-host "$($_.DriveLetter) health is in order" 
 			} else {
 				write-host "$prefix" -nonewline
-				write-host "[WARNING]:" -f yellow -nonewline 
+				write-host " [WARNING]:" -f yellow -nonewline 
 				write-host "$($_.DriveLetter) health isnt in order! Use utilits or contact a specialist.P.S. $($_.OperationalStatus)"
 			}
 		}
 	} else {
 		write-host "$prefix" -nonewline
-		write-host "[COULDN`T]:" -f red -nonewline
+		write-host " [COULDN`T]:" -f red -nonewline
 		write-host "Get your disks health status"
 	}
 } #закрыл парам
 
 if ($defrag) {
 	write-host "$prefix" -nonewline
-	write-host "[GETTING]:" -f cyan -nonewline 
+	write-host " [GETTING]:" -f cyan -nonewline 
 	write-host "Defragmentation analysis..."
-start-sleep -seconds 1
+    start-sleep -seconds 1
+    
 	if ($partitions -ne $null) {
 		$partitions | ForEach-Object {
-    			$physicalDisk = Get-PhysicalDisk -DeviceNumber $_.DiskNumber
+    		$physicalDisk = Get-PhysicalDisk -DeviceNumber $_.DiskNumber
     
-   				 if ($physicalDisk.MediaType -eq "SSD") {
-        				Write-Host "$prefix" -nonewline
-					write-host "[OK]:" -f green -nonewline
-					write-host "Drive $($_.DriveLetter): (SSD) — defragmentation not needed and might be harmful."
-    				} else {
-        				Write-Host "$prefix" -nonewline
-					write-host "[INFO]:" -f cyan -nonewline 
-					write-host "Drive $($_.DriveLetter): (HDD) — if system feels slow, run:" -nonewline
-					write-host " Optimize-Volume -DriveLetter $($_.DriveLetter) -Defrag" -f yellow
-}
-    				}
-	} write-host "$prefix" -nonewline
-	  write-host "[GOT]:" -f green -nonewline 
-	  write-host "Fragmentation information. Analysed"
-			
+   			if ($physicalDisk.MediaType -eq "SSD") {
+        		Write-Host "$prefix" -nonewline
+				write-host " [OK]:" -f green -nonewline
+				write-host "Drive $($_.DriveLetter): (SSD) — defragmentation not needed and might be harmful."
+    		} else {
+        		Write-Host "$prefix" -nonewline
+				write-host " [INFO]:" -f cyan -nonewline 
+				write-host "Drive $($_.DriveLetter): (HDD) — if system feels slow, run:" -nonewline
+				write-host " Optimize-Volume -DriveLetter $($_.DriveLetter) -Defrag" -f yellow
+            }
+        }
+        write-host "$prefix" -nonewline
+        write-host " [GOT]:" -f green -nonewline 
+        write-host "Fragmentation information. Analysed"
 	} else {
 		write-host "$prefix" -nonewline
-		write-host "[COULDN`T]:" -f red -nonewline
+		write-host " [COULDN`T]:" -f red -nonewline
 		write-host "Get fragmentation status" 
 	}
-} 
+}
+}
 
 			
 		
@@ -145,7 +146,7 @@ function Get-ServStatus {
 	if ($services.Count -gt 0) {
 		Write-Host "$prefix " -nonewline
 		write-host "[GOT]:" -f green -nonewline
-		write-host "Your services status"
+		write-host "Your service statuses"
 			$services | ForEach-Object {
 				#условия для цветов. филлер полнейший
 				if ($_.Status -eq "Running") {
@@ -178,15 +179,16 @@ function gnss {
 
 
 function get-diagerhelp {
-	write-host "$prefix [INFO] AVAILABLE CMDLETS: Get-DiagerHelp , Get-DiskStress , Get-ServStatus, Get-BatteryReport (For laptops only)" -f green
+	write-host "$prefix [INFO]: AVAILABLE CMDLETS: Get-DiagerHelp , Get-DiskStress , Get-ServStatus, Get-BatteryReport (For laptops only), Get-LagProblem." -f green
+	write-host "$prefix [INFO]: Links:" -f green
+	write-host "$prefix [INFO]: PSGallery: https://www.powershellgallery.com/packages/diager/1.0" -f green
+	write-host "$prefix [INFO]: GitHub: https://github.com/mmischab1999-sketch/diager " -f green
 }
 
 function gdh {
 	get-diagerhelp @args
 }
 
-
-$prefix = "Diager >>> "
 
 
 function get-batteryreport {
@@ -277,19 +279,109 @@ function gbr {
 }
 
 
-Export-ModuleMember -Function Get-DiskStress, Get-BatteryReport, Get-ServStatus, Get-DiagerHelp, gds, gbr, gnss, gdh
 
-$moduleVersion = (Test-ModuleManifest $PSScriptRoot\diager.psd1).Version
 		
-if (-not $script:diagerBannerShown) {
-	#=======ИМПОРТ ВЕЛКОМ МЕССЕДЖ============	
-write-host "╔═════════════════════════════════════════╗" -f cyan
-write-host "║  DIAGER v$moduleVersion-alpha Diagnostics Module   ║" -f cyan
-write-host "╚═════════════════════════════════════════╝" -f cyan
-write-host "Diager >>> Welcome! Type" -nonewline
-write-host " Get-DiagerHelp" -f yellow -nonewline
-write-host " or" -nonewline
-write-host " gdh" -f yellow -nonewline
-write-host " to see available cmdlets."
-$script:diagerBannerShown = $true
+
+
+
+
+
+function Write-Diager {
+    param(
+        [string]$Tag,          # тэг
+        [string]$Message,      # текст
+        [string]$Color = "White",  # цвет тега
+        [string]$MsgColor = "White", # цвет сообщения необязательно
+        [switch]$NoNewline
+    )
+    Write-Host "$prefix " -NoNewline
+    Write-Host "[$Tag]:" -ForegroundColor $Color -NoNewline
+    if ($MsgColor -ne "White") {
+        Write-Host " $Message" -ForegroundColor $MsgColor -NoNewline:$NoNewline
+    } else {
+        Write-Host " $Message" -NoNewline:$NoNewline
+    }
 }
+
+
+
+function Get-Lagproblem {
+	param (
+		[switch]$cpu,
+		[switch]$ram,
+		[switch]$temp,
+		[switch]$disk,
+		[switch]$battery
+	)
+
+	if (-not $cpu -and -not $ram -and -not $temp -and -not $disk) {
+		$cpu = $true
+		$ram = $true
+		$temp = $true
+		$disk = $true
+		$battery = $false
+	}
+	if ($cpu) {
+		write-diager "GETTING" "CPU Load information…" -color cyan
+		try {
+			$processorTotal = (gcim win32_processor).loadpercentage
+			write-diager "GOT" "CPU Load information." -color green
+			if ($processorTotal -le 50) {
+				write-diager "OK" "Total CPU Load: $processorTotal%" -color green
+			} elseif ($processorTotal -lt 80) {
+				write-diager "INFO" "Total CPU Load: $processorTotal%. Be careful." -color cyan
+			} elseif ($processorTotal -ge 80 -and $processorTotal -lt 95) {
+				write-diager "WARNING" "Total CPU Load: $processorTotal%. Check processes." -color yellow
+			} elseif ($processorTotal -ge 95 -and $processorTotal -le 100) {
+				write-diager "CRITICAL" "Total CPU Load: $processorTotal%. CPU maxed out!" -color red
+			}
+			$CPUTop = get-process | sort CPU -descending | select -first 5 name, cpu
+			write-diager "INFO" "Top 5 hard-loading procceses:" -color cyan
+			$CPUTop | % {
+				write-host " > $($_.Name) : $([math]::Round($_.CPU, 1))s"
+			}
+		} catch {
+			write-diager "COULDNT" "Get CPU Load information. Check, if you started the console as administrator." -color red 
+		}
+	}
+	if ($disk) {
+		Get-DiskStress -free
+	}
+
+
+	
+	if ($temp) {
+		write-diager "GETTING" "CPU Physical temperature..." -color cyan
+		try {
+			$cputemp = gcim -namespace root/wmi -className msacpi_thermalzoneTemperature -erroraction stop
+			$cputempCelsius = ($cputemp.CurrentTemperature / 10) - 273.15
+			write-diager "GOT" "CPU Physical temperature." -color green
+			if ($cputempCelsius -le 60) {
+				write-diager "OK" "CPU Temperature: $cputempCelsius°C" -color green
+			} elseif($cputempCelsius -ge 61 -and $cputempCelsius -le 85) {
+				write-diager "WARNING" "CPU Temperature: $cputempCelsius. That is hot, but its fine if you are using laptop. Check cooler if you are not." -color yellow
+			} else {
+				write-diager "CRITICAL" "CPU Temperature: $cputempCelsius. Danger zone!" -color red
+			}
+			
+		} catch {
+			write-diager "COULDNT" "Get CPU Physical temperature. Restart the console as administrator. Unless it helps - Temperature sensor not available on this system." -color red 
+		}
+	}
+	
+	if ($battery) {
+		get-batteryreport -wear
+	}
+
+}
+			
+function get-lag {
+	get-lagproblem @args
+}
+
+function glp {
+	get-lagproblem @args
+}
+
+
+Export-ModuleMember -Function Get-DiskStress, Get-BatteryReport, Get-ServStatus, Get-DiagerHelp, gds, gbr, gnss, gdh, get-lagproblem, get-lag, glp
